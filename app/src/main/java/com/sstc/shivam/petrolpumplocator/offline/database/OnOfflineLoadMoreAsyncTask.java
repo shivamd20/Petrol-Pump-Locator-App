6fb51a70e4d023385885b5fa670d8cdc63cddea9
shivamd20 <shivamd20@gmail.com>
2017-04-01 10:02:10 +0530

    adding load more option offline

---
+++ app/src/main/java/com/sstc/shivam/petrolpumplocator/offline/database/OnOfflineLoadMoreAsyncTask.java
@@ -0,0 +1,30 @@
+package com.sstc.shivam.petrolpumplocator.offline.database;
+
+import android.os.AsyncTask;
+
+/**
+ * Created by shiva on 01-04-2017.
+ */
+
+public class OnOfflineLoadMoreAsyncTask extends AsyncTask<Object, Integer, Boolean> {
+    @Override
+    protected void onPostExecute(Boolean aBoolean) {
+        super.onPostExecute(aBoolean);
+    }
+
+    @Override
+    protected void onPreExecute() {
+        super.onPreExecute();
+    }
+
+    @Override
+    protected Boolean doInBackground(Object... params) {
+        return null;
+    }
+
+    @Override
+    protected void onProgressUpdate(Integer... values) {
+        super.onProgressUpdate(values);
+    }
+}
+
